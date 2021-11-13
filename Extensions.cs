91ea723471a71e40261fef451670ddd4d6c15127
Ryan Boyer <ryanjboyer@gmail.com>
2021-11-12 23:00:52 -0700

    Add documentation

---
+++ Extensions.cs
@@ -0,0 +1,18 @@
+// Developed with love by Ryan Boyer http://ryanjboyer.com <3
+
+// For use with Timer+Async.cs
+/*using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+internal static class Extensions {
+    public static TaskAwaiter GetAwaiter(this TimeSpan timeSpan) {
+        return Task.Delay(timeSpan).GetAwaiter();
+    }
+
+    public static TaskAwaiter GetAwaiter(this Coroutine coroutine) {
+        return Task.Delay(coroutine).GetAwaiter();
+    }
+}*/
--- Timer.cs
+++ Timer.cs
@@ -4,92 +4,114 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-public class Timer : MonoBehaviour
-{
+public partial class Timer : MonoBehaviour {
     private static Timer instance = null;
-    private static Timer Shared
-    {
-        get
-        {
-            if (!(instance ?? false))
-            {
+    private static Timer Shared {
+        get {
+            if (!(instance ?? false)) {
                 CreateTimer();
             }
             return instance;
         }
     }
 
-    private void OnApplicationQuit()
-    {
+    private void OnApplicationQuit() {
         Destroy(gameObject);
         instance = null;
     }
 
     private static void CreateTimer() => instance = new GameObject("Timer").AddComponent<Timer>();
 
-    public static void Stop(Coroutine timer)
-    {
-        if (timer != null)
-        {
+    /// <summary>
+    /// Stops a timer coroutine if it's running.  The coroutine must have been started with one of the [Timer.Delay] methods.
+    /// </summary>
+    /// <param name="timer">The timer coroutine to stop.</param>
+    public static void Stop(Coroutine timer) {
+        if (timer != null) {
             Shared.StopCoroutine(timer);
         }
     }
 
+    /// <summary>
+    /// Waits until the next frame, then calls the [action].
+    /// </summary>
+    /// <param name="action">The action to do when the timer is up.</param>
+    /// <returns>Coroutine instance in case stopping is needed.</returns>
     public static Coroutine Delay(Action action) => Delay(yieldInstruction: null, action);
 
+    /// <summary>
+    /// Waits for [wait] seconds, in scaled game time, then calls the [action].
+    /// </summary>
+    /// <param name="wait">The seconds to wait for.</param>
+    /// <param name="action">The action to do when the timer is up.</param>
+    /// <returns>Coroutine instance in case stopping is needed.</returns>
     public static Coroutine Delay(float wait, Action action) => Delay(new WaitForSeconds(wait), action);
 
-    public static Coroutine Delay(float wait, bool unscaledTime, Action action)
-    {
+    /// <summary>
+    /// Waits for [wait] seconds, in optionally unscaled time, then calls the [action].
+    /// </summary>
+    /// <param name="wait">The seconds in optionally [unscaledTime] to wait for.</param>
+    /// <param name="unscaledTime">Determines if unscaled or scaled game time is used.</param>
+    /// <param name="action">The action to do when the timer is up.</param>
+    /// <returns>Coroutine instance in case stopping is needed.</returns>
+    public static Coroutine Delay(float wait, bool unscaledTime, Action action) {
         return Shared.StartCoroutine(Wait());
 
-        IEnumerator Wait()
-        {
-            if (unscaledTime)
-            {
+        IEnumerator Wait() {
+            if (unscaledTime) {
                 yield return new WaitForSecondsRealtime(wait);
-            }
-            else
-            {
+            } else {
                 yield return new WaitForSeconds(wait);
             }
             action();
         }
     }
 
-    public static Coroutine Delay(float wait, Func<float> deltaTime, Action action)
-    {
+    /// <summary>
+    /// Waits for [wait] value, increasing the timer by [delta] every frame, then calls the [action].
+    /// </summary>
+    /// <param name="wait">The value to wait for.</param>
+    /// <param name="delta">The update rate to increase the timer by every frame.</param>
+    /// <param name="action">The action to do when the timer is up.</param>
+    /// <returns>Coroutine instance in case stopping is needed.</returns>
+    public static Coroutine Delay(float wait, Func<float> delta, Action action) {
         return Shared.StartCoroutine(Wait());
 
-        IEnumerator Wait()
-        {
+        IEnumerator Wait() {
             float time = 0;
-            while (time < wait)
-            {
-                time += deltaTime();
+            while (time < wait) {
+                time += delta();
                 yield return null;
             }
             action();
         }
     }
 
-    public static Coroutine Delay(CustomYieldInstruction customYieldInstruction, Action action)
-    {
+    /// <summary>
+    /// Waits for [customYieldInstruction] to complete, then calls the [action].
+    /// </summary>
+    /// <param name="customYieldInstruction">The CustomYieldInstruction to wait for.</param>
+    /// <param name="action">The action to do when the timer is up.</param>
+    /// <returns>Coroutine instance in case stopping is needed.</returns>
+    public static Coroutine Delay(CustomYieldInstruction customYieldInstruction, Action action) {
         return Shared.StartCoroutine(Wait());
 
-        IEnumerator Wait()
-        {
+        IEnumerator Wait() {
             yield return customYieldInstruction;
             action();
         }
     }
 
-    public static Coroutine Delay(YieldInstruction yieldInstruction, Action action)
-    {
+    /// <summary>
+    /// Waits for [yieldInstruction] to complete, then calls the [action].
+    /// </summary>
+    /// <param name="yieldInstruction">The YieldInstruction to wait for.</param>
+    /// <param name="action">The action to do when the timer is up.</param>
+    /// <returns>Coroutine instance in case stopping is needed.</returns>
+    public static Coroutine Delay(YieldInstruction yieldInstruction, Action action) {
         return Shared.StartCoroutine(Wait());
 
-        IEnumerator Wait()
-        {
+        IEnumerator Wait() {
             yield return yieldInstruction;
             action();
         }
