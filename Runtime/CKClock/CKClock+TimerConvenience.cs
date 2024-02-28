dfc72dd80d700aa08ac7554a8faa88ae3e8ae044
Ryan Boyer <ryanjboyer@gmail.com>
2024-02-28 05:23:07 -0700

    additional timer convenience

--- Runtime/CKClock/CKClock+TimerConvenience.cs
+++ Runtime/CKClock/CKClock+TimerConvenience.cs
@@ -201,6 +201,45 @@ namespace ClockKit {
 		}
 
 		public static CKKey Delay(
+			CKQueue queue,
+			in CKIndefiniteDelayingTimer.CompletionPredicate until,
+			in CKIndefiniteDelayingTimer.SimpleCompletionCallback onComplete = null
+		) {
+			ICKTimer timer = new CKIndefiniteDelayingTimer(
+				startTime: Time.time,
+				onComplete: onComplete,
+				completionPredicate: until
+			);
+			return StartTimer(queue, timer);
+		}
+
+		public static CKKey Delay(
+			CKQueue queue,
+			in CKIndefiniteDelayingTimer.SimpleCompletionPredicate until,
+			in CKIndefiniteDelayingTimer.CompletionCallback onComplete = null
+		) {
+			ICKTimer timer = new CKIndefiniteDelayingTimer(
+				startTime: Time.time,
+				onComplete: onComplete,
+				completionPredicate: until
+			);
+			return StartTimer(queue, timer);
+		}
+
+		public static CKKey Delay(
+			CKQueue queue,
+			in CKIndefiniteDelayingTimer.SimpleCompletionPredicate until,
+			in CKIndefiniteDelayingTimer.SimpleCompletionCallback onComplete = null
+		) {
+			ICKTimer timer = new CKIndefiniteDelayingTimer(
+				startTime: Time.time,
+				onComplete: onComplete,
+				completionPredicate: until
+			);
+			return StartTimer(queue, timer);
+		}
+
+		public static CKKey Delay(
 			in CKIndefiniteDelayingTimer.CompletionPredicate until,
 			in CKIndefiniteDelayingTimer.CompletionCallback onComplete = null
 		)
--- Runtime/Timers/CKIndefiniteDelayingTimer.cs
+++ Runtime/Timers/CKIndefiniteDelayingTimer.cs
@@ -2,22 +2,42 @@
 
 namespace ClockKit {
 	public struct CKIndefiniteDelayingTimer : ICKTimer {
-		public delegate void CompletionCallback(in CKInstant instant);
 		public delegate bool CompletionPredicate(in CKInstant instant);
+		public delegate void CompletionCallback(in CKInstant instant);
+		public delegate bool SimpleCompletionPredicate();
+		public delegate void SimpleCompletionCallback();
 
 		public float StartTime { get; }
 
-		public readonly CompletionCallback onComplete;
 		public readonly CompletionPredicate completionPredicate;
+		public readonly CompletionCallback onComplete;
 
 		public bool IsComplete { get; private set; }
 
-		public CKIndefiniteDelayingTimer(float startTime, CompletionCallback onComplete, CompletionPredicate completionPredicate) {
+		public CKIndefiniteDelayingTimer(float startTime, CompletionPredicate completionPredicate, CompletionCallback onComplete) {
 			this.StartTime = startTime;
-			this.onComplete = onComplete;
 			this.completionPredicate = completionPredicate;
+			this.onComplete = onComplete;
 			IsComplete = false;
 		}
+
+		public CKIndefiniteDelayingTimer(float startTime, CompletionPredicate completionPredicate, SimpleCompletionCallback onComplete) : this(
+			startTime,
+			completionPredicate,
+			(in CKInstant _) => onComplete?.Invoke()
+		) { }
+
+		public CKIndefiniteDelayingTimer(float startTime, SimpleCompletionPredicate completionPredicate, CompletionCallback onComplete) : this(
+			startTime,
+			(in CKInstant _) => completionPredicate(),
+			onComplete
+		) { }
+
+		public CKIndefiniteDelayingTimer(float startTime, SimpleCompletionPredicate completionPredicate, SimpleCompletionCallback onComplete) : this(
+			startTime,
+			(in CKInstant _) => completionPredicate(),
+			(in CKInstant _) => onComplete?.Invoke()
+		) { }
 
 		public bool OnUpdate(in CKInstant instant) {
 			if (IsComplete) {
